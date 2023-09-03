420b9298b471dcd1224742a39928d501ce28391a
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-09-03 14:51:04 +0100

    Three languages

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+This sript prints three lines using the cosole log
+*/
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
