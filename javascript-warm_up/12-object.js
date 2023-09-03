fc09a6dcfb7fd6b47808aa655ba81aa89257322e
Ibukun Mary Damilola <ibukunmary98@gmail.com>
2023-09-03 13:54:05 -0700

    Changing values

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/*
+ * This scrpit reassigns a value
+*/
+
+const myObject = {
+	type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+// Update the value property
+myObject.value = 89;
+
+console.log(myObject);
