e21fe97861afdf0a5c1a7afa37a0d688556cbcd0
Ibukun Mary Damilola <ibukunmary98@gmail.com>
2023-09-05 13:21:13 -0700

    Loop to languages

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*This script print itemss using the for loop
+*/
+const lines = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
+
