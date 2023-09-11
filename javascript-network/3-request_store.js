3352732ef9c9c245495b8bce28cc5c48c21b7ab5
Ibukun Mary Damilola <ibukunmary98@gmail.com>
2023-09-11 13:28:44 -0700

    Loripsum

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,33 @@
+#!/bin/usr/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2]; // Get the URL from the command line argument
+const filePath = process.argv[3]; // Get the file path from the command line argument
+
+if (!url || !filePath) {
+  console.error('Please provide both the URL and file path as arguments.');
+  process.exit(1);
+}
+
+// Send a GET request to the specified URL
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Request failed with status code:', response.statusCode);
+    process.exit(1);
+  }
+
+  // Write the response body to the specified file (UTF-8 encoded)
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      process.exit(1);
+    }
+    console.log(`Successfully saved the content to ${filePath}`);
+  });
+});
