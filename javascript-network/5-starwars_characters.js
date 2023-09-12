9481aa482f683afab0666963cae2969fd992be41
Ibukun Mary Damilola <ibukunmary98@gmail.com>
2023-09-12 08:38:33 -0700

    Making changes

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,12 +1,12 @@
-#!/usr/bin/node
 const request = require('request');
 
 if (process.argv.length !== 3) {
-  console.error('Usage: node 4-completed_tasks.js <API URL>');
+  console.error('Usage: node 5-starwars_characters.js <Movie ID>');
   process.exit(1);
 }
 
-const apiUrl = process.argv[2];
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request.get(apiUrl, (error, response, body) => {
   if (error) {
@@ -15,20 +15,23 @@ request.get(apiUrl, (error, response, body) => {
     console.error('Error:', `HTTP Status Code: ${response.statusCode}`);
   } else {
     try {
-      const todos = JSON.parse(body);
-      const completedTasksByUser = {};
+      const movieData = JSON.parse(body);
+      const characters = movieData.characters;
 
-      todos.forEach((todo) => {
-        if (todo.completed) {
-          if (!completedTasksByUser[todo.userId]) {
-            completedTasksByUser[todo.userId] = 1;
-          } else {
-            completedTasksByUser[todo.userId]++;
+      // Function to fetch and display character names
+      function fetchCharacterName(url) {
+        request.get(url, (error, response, body) => {
+          if (!error && response.statusCode === 200) {
+            const characterData = JSON.parse(body);
+            console.log(characterData.name);
           }
-        }
-      });
+        });
+      }
 
-      console.log(completedTasksByUser);
+      // Fetch and display character names one by one
+      characters.forEach((characterUrl) => {
+        fetchCharacterName(characterUrl);
+      });
     } catch (parseError) {
       console.error('Error:', 'Failed to parse response data');
     }
