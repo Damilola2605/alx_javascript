1d42182fd8c5acc6fd25e974a66e6a5b6d7d52ce
Ibukun Mary Damilola <ibukunmary98@gmail.com>
2023-09-12 14:55:36 -0700

    I hope this correction work

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,20 +1,36 @@
 #!/usr/bin/node
-
-// import request
 const request = require('request');
 
+const apiUrl = process.argv[2]; // Get the API URL from command line argument
 
-// pass cmd line argument
-const url = process.argv[2];
-const parts = [];
-// make a GET request
-request
-.get(url+ '/'+18, {encoding: 'utf-8'})
-.on('data', function (data) {
-    parts.push(data);
-})
-.on('complete', function () {
-    response = parts;
-    const count_ = response.films;
-    console.log(count_);
-})
+if (!apiUrl) {
+  console.error('Please provide the API URL as an argument.');
+  process.exit(1);
+}
+
+// Character ID for Wedge Antilles
+const characterId = 18;
+
+// Send a GET request to the Star Wars API
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('API request failed with status code:', response.statusCode);
+    process.exit(1);
+  }
+
+  // Parse the JSON response
+  const data = JSON.parse(body);
+
+  // Filter the films where Wedge Antilles is present
+  const filmsWithWedgeAntilles = data.results.filter((film) =>
+    film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+  );
+
+  // Print the number of films
+  console.log(filmsWithWedgeAntilles.length);
+});
