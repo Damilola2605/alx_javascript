99bb25d6f302db35ec1e375c5f072f22a43ff68d
Ibukun Mary Damilola <ibukunmary98@gmail.com>
2023-09-13 03:32:41 -0700

    Star wars Wedge Antilles

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,36 +1,25 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-const apiUrl = process.argv[2]; // Get the API URL from command line argument
-
-if (!apiUrl) {
-  console.error('Please provide the API URL as an argument.');
-  process.exit(1);
-}
-
-// Character ID for Wedge Antilles
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
 const characterId = 18;
 
-// Send a GET request to the Star Wars API
+// Make a GET request to the Star Wars API
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
-    process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
+  } else if (response.statusCode !== 200) {
     console.error('API request failed with status code:', response.statusCode);
-    process.exit(1);
+  } else {
+    const films = JSON.parse(body);
+
+    // Filter the films where "Wedge Antilles" (character ID 18) is present
+    const moviesWithWedgeAntilles = films.results.filter(film => {
+      return film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
+    });
+
+    console.log(moviesWithWedgeAntilles.length);
   }
-
-  // Parse the JSON response
-  const data = JSON.parse(body);
-
-  // Filter the films where Wedge Antilles is present
-  const filmsWithWedgeAntilles = data.results.filter((film) =>
-    film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-  );
-
-  // Print the number of films
-  console.log(filmsWithWedgeAntilles.length);
 });
+
