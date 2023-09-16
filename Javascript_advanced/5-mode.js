6a5673b8c36b4d923ffa21b457eadd8d418f493b
Ibukun Mary Damilola <ibukunmary98@gmail.com>
2023-09-16 05:53:54 -0700

    The correct file

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -1,42 +1,36 @@
-#!/usr/bin/node
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Change Mode</title>
+</head>
+<body>
+  <p>Welcome Holberton!</p>
+  <button id="spookyButton">Spooky</button>
+  <button id="darkModeButton">Dark mode</button>
+  <button id="screamModeButton">Scream mode</button>
 
-function changeMode(size, weight, transform, background, color) {
-  return function () {
-    document.body.style.fontSize = size + 'px';
-    document.body.style.fontWeight = weight;
-    document.body.style.textTransform = transform;
-    document.body.style.backgroundColor = background;
-    document.body.style.color = color;
-  };
-}
+  <script>
+    function changeMode(size, weight, transform, background, color) {
+      return function () {
+        document.body.style.fontSize = size + 'px';
+        document.body.style.fontWeight = weight;
+        document.body.style.textTransform = transform;
+        document.body.style.backgroundColor = background;
+        document.body.style.color = color;
+      };
+    }
 
-function main() {
-  const spooky = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
-  const darkMode = changeMode(12, 'bold', 'capitalize', 'black', 'white');
-  const screamMode = changeMode(12, 'normal', 'lowercase', 'white', 'black');
+    function main() {
+      const spooky = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
+      const darkMode = changeMode(12, 'bold', 'capitalize', 'black', 'white');
+      const screamMode = changeMode(12, 'normal', 'lowercase', 'white', 'black');
 
-  document.getElementById('spookyButton').addEventListener('click', spooky);
-  document.getElementById('darkModeButton').addEventListener('click', darkMode);
-  document.getElementById('screamModeButton').addEventListener('click', screamMode);
-}
+      document.getElementById('spookyButton').addEventListener('click', spooky);
+      document.getElementById('darkModeButton').addEventListener('click', darkMode);
+      document.getElementById('screamModeButton').addEventListener('click', screamMode);
+    }
 
-// Create buttons for different modes
-const spookyButton = document.createElements('button');
-spookyButton.textContent = 'Spooky';
-spookyButton.id = 'spookyButton';
-
-const darkModeButton = document.createElements('button');
-darkModeButton.textContent = 'Dark mode';
-darkModeButton.id = 'darkModeButton';
-
-const screamModeButton = document.createElements('button');
-screamModeButton.textContent = 'Scream mode';
-screamModeButton.id = 'screamModeButton';
-
-// Add buttons to the body of the page
-document.body.appendChild(spookyButton);
-document.body.appendChild(darkModeButton);
-document.body.appendChild(screamModeButton);
-
-// Call the main function to set up event listeners
-main();
+    main();
+  </script>
+</body>
+</html>
